380dfc7830e5eec5cc61021c20a18949361e48c3
shahidsiddiqui786 <56132385+shahidsiddiqui786@users.noreply.github.com>
2020-07-06 12:50:07 +0530

    site modified

--- script.js
+++
@@ -1,9 +0,0 @@
- let nav = document.querySelector('.navigation_nav');
- let navbg = document.querySelector('.navigation_background');
- let menu = document.querySelector('.menu');
-
-menu.addEventListener('click', function() {
-    this.classList.toggle('active');
-     nav.classList.toggle('nav_active_nav');
-     navbg.classList.toggle('nav_active_background');
-});
